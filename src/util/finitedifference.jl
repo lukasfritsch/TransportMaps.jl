c9ec6c39752e11db7b9132ad7a1a987fadaae2ee
Lukas Fritsch <fritsch@irz.uni-hannover.de>
2025-09-19 15:08:09 +0200

    Improve efficiency of FD gradient estimation

--- src/util/finitedifference.jl
+++ src/util/finitedifference.jl
@@ -1,11 +1,18 @@
-function central_difference_gradient(fun::Function, x::AbstractArray{<:Real}, ε=1e-10)
+function central_difference_gradient(f::F, x::Vector{Float64}, ε::Float64 = √eps(Float64)) where {F<:Function}
     n = length(x)
-    ∇fun = similar(x)
-    for i in 1:n
-        e = zeros(eltype(x), n)
-        e[i] = ε
-        ∇fun[i] = (fun(x + e) - fun(x - e)) / (2ε)
-    end
+    g = similar(x, Float64)
 
-    return ∇fun
+    # Work buffers to avoid allocations
+    xp = copy(x)
+    xm = copy(x)
+
+    for i in 1:n
+        δ = ε * max(1, abs(x[i]))
+        xp[i] = x[i] + δ
+        xm[i] = x[i] - δ
+        g[i] = (f(xp) - f(xm)) / (2δ)
+        xp[i] = x[i]   # reset
+        xm[i] = x[i]
+    end
+    return g
 end
