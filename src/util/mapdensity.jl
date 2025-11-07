f77f0efefb39b99e1afb9297090b83ee5e1db7a8
Lukas Fritsch <fritsch@irz.uni-hannover.de>
2025-11-07 18:30:09 +0100

    Refactor Softplus and ShiftedELU rectifier functions for numerical stability; update KL divergence calculations to use log-density and improve initialization in adaptive transport map optimization.

--- src/util/mapdensity.jl
+++ src/util/mapdensity.jl
@@ -1,38 +1,40 @@
 struct MapTargetDensity{F,G} <: AbstractMapDensity
-    density::F
+    density::F #! should be changed to logdensity for numerical stability
     gradient_type::Symbol
-    grad_density::G
+    grad_density::G #! should be changed to logdensity gradient for numerical stability
 
-    function MapTargetDensity(density::F, grad_density::G) where {F,G}
-        return new{F,G}(density, :analytical, grad_density)
+    #! take logdensity instead of density for numerical stability
+    function MapTargetDensity(logdensity::F, grad_density::G) where {F,G}
+        return new{F,G}(logdensity, :analytical, grad_density)
     end
 
-    function MapTargetDensity(density::F, gradient_type::Symbol, grad_density::G) where {F,G}
+    function MapTargetDensity(logdensity::F, gradient_type::Symbol, grad_density::G) where {F,G}
 
         if gradient_type ∉ [:analytical, :analytic]
             throw(ArgumentError("gradient_type must be :analytical (or :analytic) when providing a custom gradient function."))
         end
 
-        return new{F,G}(density, gradient_type, grad_density)
+        return new{F,G}(logdensity, gradient_type, grad_density)
     end
 
-    function MapTargetDensity(density::F, gradient_type::Symbol) where {F}
+    function MapTargetDensity(logdensity::F, gradient_type::Symbol) where {F}
         # Check gradient type and set gradient function accordingly
         if gradient_type ∈ [:auto_diff, :autodiff, :ad, :automatic, :forward_diff, :forwarddiff]
-            grad_density = x -> ForwardDiff.gradient(density, x)
+            grad_density = x -> ForwardDiff.gradient(logdensity, x)
             gradient_type = :auto_diff
         elseif gradient_type ∈ [:finite_difference, :finitedifference, :finite_diff, :finitediff, :fd, :numerical, :numeric]
-            grad_density = x -> central_difference_gradient(density, x)
+            grad_density = x -> central_difference_gradient(logdensity, x)
             gradient_type = :finite_difference
         else
             throw(ArgumentError("gradient_type must be either :auto_diff (or :autodiff, :ad,
             :automatic, :forward_diff) or :finite_difference (or :fd, :finite_diff, :finitediff, :numerical)."))
         end
 
-        return new{F,typeof(grad_density)}(density, gradient_type, grad_density)
+        return new{F,typeof(grad_density)}(logdensity, gradient_type, grad_density)
     end
 end
 
+#! maybe also replace density with logdensity here for numerical stability
 struct MapReferenceDensity{F, G} <: AbstractMapDensity
     density::F
     gradient_type::Symbol
